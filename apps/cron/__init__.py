b9515bcd114cd39d8450511ee45bc862389e6e46
윤정현 <wyun13043@gmail.com>
2026-01-17 14:24:16 +0900

    Add cron in apps

--- apps/cron/__init__.py
+++ apps/cron/__init__.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 scheduler = AsyncIOScheduler(timezone='Asia/Seoul')
 
 
-@scheduler.scheduled_job('cron', second='10', id='cron_test_01')
+@scheduler.scheduled_job('cron', second='*/10', id='cron_test_01')
 async def cron_test_01():
-    pass
+    print(1)
